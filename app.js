829525bfc80e0a8ec093541dde29ef4857b04b8f
HABUMUGISHA Fabrice <fabrice.habumugisha@solidor.malo>
2019-11-13 10:15:18 +0100

    initialize calcapp api

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
- const express = require('express');
- const bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
- //Database
- const mongoose = require('mongoose');
- const MONGODB_URI = "mongodb://localhost:27017/calcapp";
+//Database
+const mongoose = require('mongoose');
+const MONGODB_URI = "mongodb://localhost:27017/calcapp";
+
+//The app
 const app = express();
 
 //Parse the request body
@@ -12,39 +14,39 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
-      'Access-Control-Allow-Methods',
-      'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+        'Access-Control-Allow-Methods',
+        'OPTIONS, GET, POST, PUT, PATCH, DELETE'
     );
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
-  });
+});
 
-  //Default port
+//Default port
 const port = 3000;
 
 //Routes
 app.get('/', (req, res) => {
     res.status(200).json('GET request to the homepage')
-  })
+})
 
 //Error handler
 app.use((error, req, res, next) => {
     console.log(error);
-    const status =  error.statusCode || 500;
+    const status = error.statusCode || 500;
     const message = error.message;
-    const data =  error.data;
-    res.status(status).json({message : message, data: data});
+    const data = error.data;
+    res.status(status).json({ message: message, data: data });
 });
 
 
 //Connect to database and start server
 mongoose.connect(MONGODB_URI)
-.then(result => {
-    app.listen(port);
-})
-.catch(error => {
-    console.log(error);
-})
+    .then(result => {
+        app.listen(port);
+    })
+    .catch(error => {
+        console.log(error);
+    });
 
 
 
