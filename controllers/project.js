6ad8953c8505a947717489601cc9c002654e9b57
HABUMUGISHA Fabrice <fabrice.habumugisha@solidor.malo>
2019-11-14 13:33:21 +0100

    initialize calcapp edit categories

--- controllers/project.js
+++ controllers/project.js
@@ -1,17 +1,19 @@
-const { validationResult } = require("express-validator");
-const  Project  = require("../models/project");
-const  User  = require("../models/user");
+const {
+  validationResult
+} = require("express-validator");
+const Project = require("../models/project");
+const User = require("../models/user");
 
 exports.getProjects = async (req, res, next) => {
- 
   const currentPage = req.query.page || 1;
   const perPage = 2;
-
   try {
     const totalItems = await Project.find({
       userId: req.userId
     }).countDocuments();
-    const projects = await Project.find({ userId: req.userId })
+    const projects = await Project.find({
+        userId: req.userId
+      })
       .skip((currentPage - 1) * perPage)
       .limit(perPage);
 
@@ -29,101 +31,265 @@ exports.getProjects = async (req, res, next) => {
 };
 
 exports.postProject = async (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      const error = new Error('Validation failed, entered data is incorrect.');
-      error.statusCode = 422;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed, entered data is incorrect.');
+    error.statusCode = 422;
+    throw error;
+  }
+
+  const title = req.body.title;
+  const type = req.body.type;
+  const totalAmount = 0;
+  const project = new Project({
+    title: title,
+    type: type,
+    userId: req.userId,
+    totalAmount: totalAmount
+  });
+  try {
+    const createdProject = await project.save();
+    const user = await User.findById(req.userId);
+    user.projects.push(createdProject);
+    await user.save();
+    res.status(200).json({
+      message: 'Project created successfully',
+      project: createdProject
+    });
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  }
+
+};
+
+exports.updateProject = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed, entered data is incorrect.');
+    error.statusCode = 422;
+    throw error;
+  }
+  const projectId = req.params.projectId;
+  try {
+    const project = await verifyUserData(projectId, req);
+    const title = req.body.title;
+    //const type =  req.body.type;
+    project.title = title;
+    // project.type =  type;
+    const updatedProject = await project.save();
+    res.status(200).json({
+      message: 'Project updated!',
+      project: updatedProject
+    });
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  }
+
+
+};
+
+exports.deleteProject = async (req, res, next) => {
+  const projectId = req.params.projectId;
+
+  try {
+    await verifyUserData(projectId, req);
+
+    await Project.findByIdAndRemove(projectId);
+
+    const user = await User.findById(req.userId);
+    user.projects.pull(projectId);
+    await user.save();
+    res.status(200).json({
+      message: 'Deleted project.'
+    });
+
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  }
+
+
+};
+
+exports.getProject = async (req, res, next) => {
+  const projectId = req.params.projectId;
+
+  try {
+    const project = await verifyUserData(projectId, req);
+    res.status(200).json({
+      message: 'Project fetched successfully updated!',
+      project: project
+    });
+
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  }
+};
+
+exports.getProjectCategories = async (req, res, next) => {
+  const projectId = req.params.projectId;
+
+  try {
+    const project = await verifyUserData(projectId, req);    
+    res.status(200).json({
+      message: 'Project categories fetched successfully !',
+      categories: project.categories
+    });
+
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  }
+};
+
+exports.getProjectCategory = async (req, res, next) => {
+  const projectId = req.params.projectId;
+  const categoryId = req.params.categoryId;
+  try {
+    const project = await verifyUserData(projectId, req);  
+    
+    const category = project.categories.id(categoryId);
+    if (!category) {
+      const error = new Error('Could not find category.');
+      error.statusCode = 404;
       throw error;
     }
-
-    const title = req.body.title;
-    const type =  req.body.type;
-    const totalAmount =  0;
-    const project = new Project({
-        title: title,
-        type: type,
-        userId : req.userId,
-        totalAmount : totalAmount
+    res.status(200).json({
+      message: 'Project category fetched successfully !',
+      category: category
     });
-    try {
-        const createdProject = await project.save();
-        const user = await User.findById(req.userId);
-        user.projects.push(createdProject);
-        await user.save();
-        res.status(200).json({
-            message: 'Project created successfully',
-            project : createdProject
-        }); 
-    } catch (error) {
-        if (!error.statusCode) {
-            error.statusCode = 500;
-          }
-          next(error);
+
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  }
+};
+
+
+exports.createProjectCategory = async (req, res, next) => {
+  const projectId = req.params.projectId;
+  try {
+    const project = await verifyUserData(projectId, req);
+    const title = req.body.title;
+    const type = req.body.type;
+    const category = {
+      title: title,
+      type: type
+    }
+
+    project.categories.push(category);
+    const updatedProject = await project.save();
+    res.status(200).json({
+      message: 'Category created successfully !',
+      project: updatedProject
+    });
+
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  }
+};
+
+exports.updateProjectCategory = async (req, res, next) => {
+  const projectId = req.params.projectId;
+  const categoryId = req.params.categoryId;
+  try {
+    const project = await verifyUserData(projectId, req);
+    const category = project.categories.id(categoryId);
+    if (!category) {
+      const error = new Error('Could not find category.');
+      error.statusCode = 404;
+      throw error;
+    }
+    const title = req.body.title;
+    
+    
+    category.title = title;
+    console.log(category);
+    const updatedProject = await project.save();
+    res.status(200).json({
+      message: 'Category updated successfully !',
+      project: updatedProject
+    });
+
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  }
+};
+
+
+
+exports.deleteProjectCategory = async (req, res, next) => {
+  const projectId = req.params.projectId;
+  const categoryId = req.params.categoryId;
+  try {
+    const project = await verifyUserData(projectId, req);
+    const category = project.categories.id(categoryId);
+    if (!category) {
+      const error = new Error('Could not find category.');
+      error.statusCode = 404;
+      throw error;
+    }
+    project.categories.pull(categoryId);
+    const updatedProject = await project.save();
+    res.status(200).json({
+      message: 'Category deleted successfully !',
+      project: updatedProject
+    });
+
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  }
+};
+
+
+
+exports.getProjectCategoryItems = async (req, res, next) => {
+  const projectId = req.params.projectId;
+  const categoryId = req.params.categoryId;
+  try {
+    const project = await verifyUserData(projectId, req);
+    const category = project.categories.id(categoryId);
+    if (!category) {
+      const error = new Error('Could not find category.');
+      error.statusCode = 404;
+      throw error;
     }
     
-};
+    res.status(200).json({
+      message: 'Category items fetched successfully !',
+      items: category.items
+    });
 
-exports.updateProject = (req, res, next) => {
-  res.status(200).json({
-    title: "Let go!",
-    totalPrice: "200 Euros"
-  });
-};
-
-exports.deleteProject = (req, res, next) => {
-  res.status(200).json({
-    title: "Let go!",
-    totalPrice: "200 Euros"
-  });
-};
-
-exports.getProject = (req, res, next) => {
-  res.status(200).json({
-    title: "Let go!",
-    totalPrice: "200 Euros"
-  });
-};
-
-exports.getProjectCategories = (req, res, next) => {
-  res.status(200).json({
-    title: "Let go!",
-    totalPrice: "200 Euros"
-  });
-};
-
-exports.getProjectCategory = (req, res, next) => {
-  res.status(200).json({
-    title: "Let go!",
-    totalPrice: "200 Euros"
-  });
-};
-
-exports.createProjectCategory = (req, res, next) => {
-  res.status(200).json({
-    title: "Let go!",
-    totalPrice: "200 Euros"
-  });
-};
-
-exports.updateProjectCategory = (req, res, next) => {
-  res.status(200).json({
-    title: "Let go!",
-    totalPrice: "200 Euros"
-  });
-};
-
-exports.deleteProjectCategory = (req, res, next) => {
-  res.status(200).json({
-    title: "Let go!",
-    totalPrice: "200 Euros"
-  });
-};
-
-exports.getProjectCategoryItems = (req, res, next) => {
-  res.status(200).json({
-    title: "Let go!",
-    totalPrice: "200 Euros"
-  });
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  }
 };
 
 exports.getProjectCategoryItem = (req, res, next) => {
@@ -188,3 +354,19 @@ exports.updateProjectItem = (req, res, next) => {
     totalPrice: "200 Euros"
   });
 };
+
+
+async function verifyUserData(projectId, req) {
+  const project = await Project.findById(projectId);
+  if (!project) {
+    const error = new Error('Could not find project.');
+    error.statusCode = 404;
+    throw error;
+  }
+  if (project.userId.toString() != req.userId) {
+    const error = new Error('Not authorized!');
+    error.statusCode = 403;
+    throw error;
+  }
+  return project;
+}
--- routes/project.js
+++ routes/project.js
@@ -1,8 +1,10 @@
 const express = require('express');
 
-const { body} = require('express-validator');
+const {
+    body
+} = require('express-validator');
 
-const auth  = require('../middleware/auth');
+const auth = require('../middleware/auth');
 
 const projectController = require('../controllers/project');
 
@@ -10,34 +12,44 @@ const router = express.Router();
 
 //Projects
 router.get('/', auth, projectController.getProjects);
+
 router.post('/', auth, [
-    body('title').exists(),
-    body('type').exists()
-],
- projectController.postProject);
+        body('title').exists(),
+        body('type').exists()
+    ],
+    projectController.postProject);
+
 router.get('/:projectId', auth, projectController.getProject);
-router.put('/:projectId', auth, projectController.updateProject);
+
+router.put('/:projectId', auth, [
+    body('title').exists()
+], projectController.updateProject);
+
 router.delete('/:projectId', auth, projectController.deleteProject);
 
 //Categories
 router.get('/:projectId/categories', auth, projectController.getProjectCategories);
-router.post('/:projectId/categories',auth, projectController.createProjectCategory);
-router.get('/:projectId/categories/:categId',auth, projectController.getProjectCategory);
-router.delete('/:projectId/categories/:categId',auth, projectController.deleteProjectCategory);
-router.put('/:projectId/categories/:categId',auth, projectController.updateProjectCategory);
+
+router.post('/:projectId/categories', auth, projectController.createProjectCategory);
+
+router.get('/:projectId/categories/:categoryId', auth, projectController.getProjectCategory);
+
+router.delete('/:projectId/categories/:categoryId', auth, projectController.deleteProjectCategory);
+
+router.put('/:projectId/categories/:categoryId', auth, projectController.updateProjectCategory);
 
 //Category Items
-router.get('/:projectId/categories/:categId/items',auth, projectController.getProjectCategoryItems);
-router.post('/:projectId/categories/:categId/items',auth, projectController.createProjectCategoryItem);
-router.get('/:projectId/categories/:categId/items/:itemId',auth, projectController.getProjectCategoryItem);
-router.delete('/:projectId/categories/:categId/items/:itemId',auth, projectController.deleteProjectCategoryItem);
-router.put('/:projectId/categories/:categId/items/:itemId',auth, projectController.updateProjectCategoryItem);
+router.get('/:projectId/categories/:categoryId/items', auth, projectController.getProjectCategoryItems);
+router.post('/:projectId/categories/:categoryId/items', auth, projectController.createProjectCategoryItem);
+router.get('/:projectId/categories/:categoryId/items/:itemId', auth, projectController.getProjectCategoryItem);
+router.delete('/:projectId/categories/:categoryId/items/:itemId', auth, projectController.deleteProjectCategoryItem);
+router.put('/:projectId/categories/:categoryId/items/:itemId', auth, projectController.updateProjectCategoryItem);
 
 //Project Items
-router.get('/:projectId/items',auth, projectController.getProjectItems);
-router.post('/:projectId/items', auth,projectController.createProjectItem);
-router.get('/:projectId/items/:itemId', auth,projectController.getProjectItem);
-router.delete('/:projectId/items/:itemId',auth, projectController.deleteProjectItem);
-router.put('/:projectId/items/:itemId', auth,projectController.updateProjectItem);
+router.get('/:projectId/items', auth, projectController.getProjectItems);
+router.post('/:projectId/items', auth, projectController.createProjectItem);
+router.get('/:projectId/items/:itemId', auth, projectController.getProjectItem);
+router.delete('/:projectId/items/:itemId', auth, projectController.deleteProjectItem);
+router.put('/:projectId/items/:itemId', auth, projectController.updateProjectItem);
 
 module.exports = router;
